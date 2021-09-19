6f115a74a61807501816548faf77a9cc35b2cedf
Juliano Amorim <julianoamorim89@outlook.com>
2021-09-19 18:51:16 -0300

    Atualizacao lazy loading login

--- src/app/folder/components/home/home.page.ts
+++ src/app/folder/components/home/home.page.ts
@@ -2,6 +2,9 @@ import { CredenciaisDTO } from './../../../../models/credenciais.dto';
 import { Component, OnInit } from '@angular/core';
 import { LoadingController, MenuController, NavController } from '@ionic/angular'; //controle de navegacao
 import { AuthService } from 'src/services/auth.service';
+import { from, Observable } from 'rxjs';
+import { finalize, flatMap, tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -15,6 +18,8 @@ export class HomePage implements OnInit {
     email: "",
     senha: ""
   }
+
+  isLoading: boolean = false; //variavel para verificar se a pagina esta carregando ou nao
 
   constructor(public navCtrl: NavController,
     public menu: MenuController,
@@ -39,13 +44,15 @@ export class HomePage implements OnInit {
     }
 
   login(){
+    
     this.auth.autenticado(this.creds)
     .subscribe(resposta => {
+      this.showLoader();
       this.auth.sucessoLogin(resposta.headers.get('Authorization')); //consegue pegar o token pelo header no backend no arquivo JWTFiltroAutenticacao
+      this.isLoading = false; //desabilita o loading apos a resposta HTTPS
       this.navCtrl.navigateRoot('folder/components/categorias'); //navega da home para categoria
     },
     error => {});
-    this.showLoader();
     
     //this.navCtrl.navigateForward('folder/components/categorias') //permite voltar para essa pagina
   }
@@ -55,14 +62,17 @@ export class HomePage implements OnInit {
   }
 
   async showLoader(){ //metodo usado em requisicoes q podem demorar, gerando a tela de Loading...
+    this.isLoading = true;
     const loading = await this.loadingCtrl.create({
       cssClass: 'my-custom-class',
       message: 'Aguarde...',
-      duration: 500
+      duration: 5000
     });
-    await loading.present();
-
-    const { role, data } = await loading.onDidDismiss();
+    await loading.present().then(() => {
+      console.log('carregando');
+      if(!this.isLoading)
+        loading.dismiss().then(() => console.log('Dismiss'))
+    })
   }
 
   ngOnInit() {

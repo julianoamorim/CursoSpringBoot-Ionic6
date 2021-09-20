f601561b0fd07e0b0dbfb201ff10881a3453e4f0
Juliano Amorim <julianoamorim89@outlook.com>
2021-09-20 19:18:13 -0300

    correcao pagina registro

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl : "https://localhost:8080", //Endereco BackEnd SpringBoot, no localhost do Java ou heroku/ usar HTTPS
+    baseUrl : "https://curso-spring-ionic15.herokuapp.com", //Endereco BackEnd SpringBoot, no localhost do Java ou heroku/ usar HTTPS
     bucketAWSUrl: "https://curso-spring-ionic15.s3.sa-east-1.amazonaws.com" //URL para acesso as imagens no AWS
 }
